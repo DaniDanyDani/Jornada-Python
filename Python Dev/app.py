d8e935e79dbb60d6ab85117c75ec1b741db1d75d
Daniel Moreira <daniel.moreira.leme@gmail.com>
2024-01-18 15:24:17 -0300

    Chat updates

--- Python Dev/app.py
+++ Python Dev/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template # Estruturas para criar o site
 from flask_socketio import SocketIO, send # estruturas para criar o chat
 
-chave_seguranca = "dinx0ej23mrce-0,mrx0-2,r-029,0@#$%¨&*()"
-
 app = Flask(__name__)
-app.config["SECRET"] = chave_seguranca
+app.config["SECRET"] = "dinx0ej23mrce-0,mrx0-2,r-029,0@#$%¨&*()"
 app.config["DEBUG"] = True
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -18,4 +16,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host='localhost')
+    socketio.run(app, host='localhost', port=5000)
