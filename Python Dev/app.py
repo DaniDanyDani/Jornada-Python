fa3c536f5e41535a1aa0ff567ab111997630e488
Daniel Moreira <daniel.moreira.leme@gmail.com>
2024-01-18 15:26:49 -0300

    Revert "Chat updates"
    This reverts commit d8e935e79dbb60d6ab85117c75ec1b741db1d75d.

--- Python Dev/app.py
+++ Python Dev/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template # Estruturas para criar o site
 from flask_socketio import SocketIO, send # estruturas para criar o chat
 
+chave_seguranca = "dinx0ej23mrce-0,mrx0-2,r-029,0@#$%¨&*()"
+
 app = Flask(__name__)
-app.config["SECRET"] = "dinx0ej23mrce-0,mrx0-2,r-029,0@#$%¨&*()"
+app.config["SECRET"] = chave_seguranca
 app.config["DEBUG"] = True
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -16,4 +18,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host='localhost', port=5000)
+    socketio.run(app, host='localhost')
